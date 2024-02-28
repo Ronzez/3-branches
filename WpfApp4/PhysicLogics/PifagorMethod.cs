b2a2a2be744a54b53dfde611b1fdcfe40a81647e
Ronzez <ronzez54@gmail.com>
2024-02-28 15:54:31 +0300

    Added hypotenuse logic

--- WpfApp4/PhysicLogics/PifagorMethod.cs
+++ WpfApp4/PhysicLogics/PifagorMethod.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace WpfApp4.PhysicLogics
 {
-    internal class PifagorMethod
+    public class PifagorMethod
     {
+        static double CalculateHypotenuse(int a, int b, int angle)
+        {
+            double c = Math.Pow(a, 2) + Math.Pow(b, 2) - (2 * b * a) * Math.Cos(angle);
+            return Math.Sqrt(c);
+        }
     }
 }
